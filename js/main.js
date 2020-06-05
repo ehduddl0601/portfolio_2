369c66df3fe9741b190f8cb6fd6ae5415b66c129
ehduddl0601 <63777060+ehduddl0601@users.noreply.github.com>
2020-06-05 14:13:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -87,4 +87,15 @@ $(function(){
             });
         }
     });
+    var click_num = 0;
+    $('#btn').on('click', function(){
+        click_num++;
+        console.log(click_num % 2);
+        if(click_num % 2 == 0){
+            $('#menu').css('top', '0')
+        }
+        else if(click_num % 2 == 1){
+            $('#menu').css('top', '110px')
+        };
+    });
 });
